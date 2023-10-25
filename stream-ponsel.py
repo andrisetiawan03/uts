2ffbf7dfd4c6b3c7973e7732cab247cd3436ab01
andrisetiawan03 <148999404+andrisetiawan03@users.noreply.github.com>
2023-10-25 23:53:41 +0700

    Add files via upload

---
+++ stream-ponsel.py
@@ -0,0 +1,24 @@
+import pickle
+import streamlit as st
+
+ponsel_model = pickle.load(open('estimasi_harga_HP.sav', 'rb'))
+
+st.title('Estimasi Harga HP Berdasarkan Spesifikasi')
+
+RAM = st.number_input('Kapasitas RAM (GB)')
+Cpu_Core = st.number_input('Jumlah Core CPU')
+Internal = st.number_input('Total Memory Internal (GB)')
+Battery = st.number_input('Kapasitas Baterai (mAh)')
+FrontCam = st.number_input('Kamera Depan (Mega Pixels)')
+RearCam = st.number_input('Kamera Belakang (Mega Pixels)')
+Resolution = st.number_input('Ukuran Layar (INCH)')
+Cpu_Freq = st.number_input('Kecepatan CPU (Ghz)')
+
+predict = ''
+
+if st.button('Estimasi'):
+    predict = ponsel_model.predict(
+        [[RAM, Cpu_Core, Internal, Battery, FrontCam, RearCam, Resolution, Cpu_Freq]]
+    )
+    st.write ('Estimasi harga dalam MYR : ', predict)
+    st.write ('Estimasi dalam Rupiah : ', predict*3319)
